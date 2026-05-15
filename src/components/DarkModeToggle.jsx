e0211bf25b7e43bcfd7753300e85d0b6611c64c6
Lucy <lucymwago9@gmail.com>
2026-05-16 00:29:16 +0300

    run tests

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const DarkModeToggle = () => {
+const DarkModeToggle = ({darkMode, setDarkMode}) => {
   // TODO: Implement dark mode toggle logic
 
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
+    <button onClick={() => setDarkMode(!darkMode)}>Toggle {darkMode ? 'Light Mode' : 'Dark Mode'}</button>
   )
 }
 
